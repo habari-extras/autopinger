22d73d2f90331bcb7fda04640b0133c757cfd747
Chris Meller <chris@doesnthaveone.com>
2013-02-12 11:57:44 -0600

    Fix HabariDateTime calls.

--- autopinger.plugin.php
+++ autopinger.plugin.php
@@ -12,8 +12,8 @@ class Autopinger extends Plugin
 	 */
 	public function action_post_status_published( $post)
 	{
-		if ( $post->status == Post::status( 'published' ) && $post->pubdate <= HabariDateTime::date_create() ) {
-			CronTab::add_single_cron( 'ping update sites', array( 'Autopinger', 'ping_sites' ), HabariDateTime::date_create()->int, 'Ping update sites.' );
+		if ( $post->status == Post::status( 'published' ) && $post->pubdate <= DateTime::create() ) {
+			CronTab::add_single_cron( 'ping update sites', array( 'Autopinger', 'ping_sites' ), DateTime::create()->int, 'Ping update sites.' );
 			EventLog::log( 'Crontab added', 'info', 'default', null, null );
 		}
 	}
